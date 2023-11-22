fe2b59a03873ebc7e7b92ed325111ac472f7ef09
Николай Демко <dnvcomp@yandex.ru>
2023-11-22 17:13:35 +0300

    Insert data in table category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use Auth;
+use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -12,6 +15,7 @@ class CategoryController extends Controller
     }
     public function addCategory(Request $request)
     {
+        // Ограничение символов в тексте
         $validate = $request->validate([
             'category_name'=>'required|unique:categories|max:15',
             'category_name'=>'required|unique:categories|min:5',
@@ -20,5 +24,20 @@ class CategoryController extends Controller
             'category_name.max'=>'Название категории должно быть не больше 15 символов.',
             'category_name.min'=>'Название категории должно быть не меньше 5 символов.',
         ]);
+        // Вставка данных в таблицу Категории базы данных
+
+        // Вариант - 1
+        // Category::insert([
+        //    'category_name'=>$request->category_name,
+        //    'user_id'=>Auth::user()->id,
+        //    'created_at'=>Carbon::now(),
+        //]);
+
+        // Вариант - 2
+        $category = new Category;
+        $category->category_name = $request->category_name;
+        $category->user_id = Auth::user()->id;
+        $category->save();
+        return Redirect()->back()->with('success','Название категории успешно создано');
     }
 }
